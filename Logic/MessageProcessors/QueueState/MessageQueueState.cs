4f0a0e6d8d6c7878a0bccb00cbf4623f77254388
Renat <renatmusaev@yandex.ru>
2021-02-22 01:05:22 +0400

    add comments and constants

--- Logic/MessageProcessors/QueueState/MessageQueueState.cs
+++ Logic/MessageProcessors/QueueState/MessageQueueState.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections.Concurrent;
-using System.Threading;
+﻿using System.Threading;
 
 namespace Logic.MessageProcessors.QueueState
 {
@@ -8,20 +7,31 @@ namespace Logic.MessageProcessors.QueueState
     /// </summary>
     public class MessageQueueState : INewMessageInformer, INewMessageReceiver
     {
-        private int _wasAdded = 0;
+        private const int NO_MESSAGES = 0;
+        private const int MESSAGE_WAS_ADDED = 1;
+
+        /// <summary>
+        /// <see cref="NO_MESSAGES"/> is no messages after <see cref="WasNewMessage"/> 
+        /// <see cref="MESSAGE_WAS_ADDED"/> is was message (by <see cref="MessageWasAdded"/>)
+        /// </summary>
+        private int _wasAdded = NO_MESSAGES;
 
         public void MessageWasAdded()
         {
-            Interlocked.Exchange(ref _wasAdded, 1);
+            Interlocked.Exchange(ref _wasAdded, MESSAGE_WAS_ADDED);
         }
 
         public bool WasNewMessage()
         {
-            if (Interlocked.CompareExchange(ref _wasAdded, 0, 1)==1)
+            //set 0 if was 1
+            if (Interlocked.CompareExchange(ref _wasAdded, NO_MESSAGES, MESSAGE_WAS_ADDED) 
+                == MESSAGE_WAS_ADDED)
             {
+                //was new messages
                 return true;
             }
 
+            //wasn't new messages
             return false;
         }
     }
